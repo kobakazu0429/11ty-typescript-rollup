a5afc388c78011f30ebd784080d90b13e661ce5a
kobakazu0429 <kobakazu10@gmail.com>
2021-08-03 21:41:33 +0900

    eleventyConfig.setUseGitIgnore(false)

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = function (eleventyConfig) {
+  /**
+   * .js files are generated to /build by rollup
+   * but eleventy is ignore .gitignore automatically
+   */
+  eleventyConfig.setUseGitIgnore(false);
+
   return {
     dir: {
       input: "./build",
